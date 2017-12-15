650cdb79464c4d12ff75ecf6a663378016da9ce3
UltraNote <31513990+UltraNote@users.noreply.github.com>
2017-12-15 15:06:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# UltraNote-Universal-Pool
---
+++ UltraNote-Universal-Pool-master/website/config.js
@@ -0,0 +1,17 @@
+var api = "http://alpha.ultranote.org:8117";
+
+var poolHost = "alpha.ultranote.org";
+
+var irc = "irc.freenode.net/#UltraNote";
+
+var email = "support@ultranote.org";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://explorer.ultranote.org/?hash={id}#blockchain_block";
+
+var transactionExplorer = "http://explorer.ultranote.org/?hash={id}#blockchain_transaction";
+
+var themeCss = "default-theme.css";
